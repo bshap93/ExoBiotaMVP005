9c1642ee134e03d88948fa4967b70e84968e6272
Ben Shapiro <bshap93@gmail.com>
2026-02-09 17:41:45 -0700

    Meta terminasl

---
+++ Assets/Scripts/FirstPersonPlayer/ScriptableObjects/MetaTerminalInfoSO.cs
@@ -0,0 +1,18 @@
+﻿using EditorScripts;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FirstPersonPlayer.ScriptableObjects
+{
+    [CreateAssetMenu(
+        fileName = "MetaTerminal", menuName = "Scriptable Objects/Machines/MetaTerminal",
+        order = 1)]
+    public class MetaTerminalInfoSO : ScriptableObject
+    {
+        public string gameObjectUniqueID;
+        public string terminalName;
+
+        [SerializeField] [InlineProperty] [HideLabel]
+        SpawnInfoEditor overrideSpawnInfo;
+    }
+}
---
+++ Assets/Scripts/Manager/ProgressionMangers/TerminalManager.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager.ProgressionMangers
+{
+    public class TerminalManager : MonoBehaviour
+    {
+        HashSet<string> _visitedMetaTerminals;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        }
+    }
+}
