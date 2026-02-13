73082b0f9992edd4ea13aefe39e669ce5db947ec
Ben Shapiro <bshap93@gmail.com>
2026-02-12 17:45:41 -0700

    Foliage

--- Assets/Scripts/Helpers/Collider/LiquidEffectTrigger.cs
+++ Assets/Scripts/Helpers/Collider/LiquidEffectTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using Helpers.Events.Status;
+using Manager;
 using UnityEngine;
 
 namespace Helpers.Collider
@@ -24,9 +25,17 @@ namespace Helpers.Collider
                 switch (liquidType)
                 {
                     case LiquidType.HotspringPoolWater:
-                        PlayerStatsEvent.Trigger(
-                            PlayerStatsEvent.PlayerStat.CurrentHealth, PlayerStatsEvent.PlayerStatChangeType.Increase,
-                            20f);
+                        var isPlayerHealthMaxed = false;
+                        var statsMgr = PlayerMutableStatsManager.Instance;
+                        if (statsMgr != null)
+                            if (statsMgr.CurrentHealth >= statsMgr.CurrentMaxHealth - 0.5f)
+                                isPlayerHealthMaxed = true;
+
+                        if (!isPlayerHealthMaxed)
+                            PlayerStatsEvent.Trigger(
+                                PlayerStatsEvent.PlayerStat.CurrentHealth,
+                                PlayerStatsEvent.PlayerStatChangeType.Increase,
+                                20f);
 
                         break;
 
