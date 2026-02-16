99bf5798c66fb5127be9c7d0cf2c762e4cd02442
Ben Shapiro <bshap93@gmail.com>
2026-02-16 12:55:26 -0700

    Gate opens

--- Assets/Scripts/Manager/PlayerSpawnManager.cs
+++ Assets/Scripts/Manager/PlayerSpawnManager.cs
@@ -247,7 +247,7 @@ namespace Manager
                 "chapel_center", "chapel_exit", "hotspring_house", "WestEntrySpawnHSAdjCorr",
                 "SouthEntrySpawnHSAdjCorr",
                 "StartSpawn", "TerminalSpawn00", "FacilityUtilityC2Spawn", "DownwardPsgTopSpawn",
-                "BattlefieldGateSpawn"
+                "BattlefieldGateSpawn", "hotspring_gate",
             };
         }
 
